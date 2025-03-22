124429cfcf550cb82a8118ede0ba643164830ea8
Rajat Kumar <rajat.kumar@coxautoinc.com>
2025-03-22 15:04:36 +0530

    Update Main.java

--- Practice-Java/src/leetcode/Main.java
+++ Practice-Java/src/leetcode/Main.java
@@ -1,8 +1,27 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+
+    public int[] twoSum(int[] nums, int target)
+    {
+        Map<Integer, Integer> nums_map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            int temp = target - nums[i];
+            if(nums_map.containsKey(temp))
+            {
+                return new int[] {nums_map.get(temp), i};
+            }
+            nums_map.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No match found!");
+    }
 
     public static void main(String[] args) {
 
